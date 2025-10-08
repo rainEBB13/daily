beae3d6ab49aed1731cca14b10b2b4df9e38cdc0
Your Name <rainneeliot13@gmail.com>
2025-10-07 20:46:48 -0700

    Added a main.java file for midterm review for CSE 110 class

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String s1 = "a banana";
+        String s2 = "banana";
+
+        int result = s1.compareTo(s2);
+        System.out.println(result);
+    }
+}
