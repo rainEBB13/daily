1f9dad465506ab21fa60596a36800c48fd310407
Your Name <rainneeliot13@gmail.com>
2025-10-13 13:42:11 -0700

    added raining.py file

---
+++ raining.py
@@ -0,0 +1,9 @@
+def rainGreeting():
+    print("Rain rain go away")
+
+def rainGreeting2():
+    print("Come again another day!")
+
+# print(rainGreeting() + " " + rainGreeting2)
+rainGreeting() 
+rainGreeting2()
